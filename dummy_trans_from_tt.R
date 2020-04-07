628acd5b61bc9be3d6f2509da4ad3bdff2688afd
aclong <37698923+aclong@users.noreply.github.com>
2020-04-07 16:26:09 +0100

    actually starting code

--- dummy_trans_from_tt.R
+++ dummy_trans_from_tt.R
@@ -44,3 +44,40 @@
 ## upload a full version to SQL db
 ## upload a random sample of 20% of rows (to approximate transaction coverage) to another SQL db
 
+#what time period?
+
+#going to start with september to november 2015
+
+start_date <- "2015-09-01"
+end_date <- "2015-11-30"
+
+#generate list
+dates <- seq(as.Date(start_date), as.Date(end_date), by="days")
+
+#load in the packages needed
+library(data.table)
+library(RPostgreSQL)
+library(lubridate)
+library(glue)
+
+#create db connection
+#load in the env variables
+db_name=Sys.getenv("DBNAME")
+host_name=Sys.getenv("HOSTNAME")
+user_name=Sys.getenv("USERNAME")
+password_name=Sys.getenv("PASSWORDNAME")
+
+
+#set up a connection
+con <- dbConnect(dbDriver("PostgreSQL"), 
+                 dbname = db_name, 
+                 host = host_name, 
+                 user = user_name, 
+                 password = password_name)
+
+#get all operators/routes from this period
+operator_route_list <- dbGetQuery(con, glue("SELECT operator, route FROM timetables.tt_all WHERE start_date<'{end_date}' AND last_date>'{start_date}' GROUP BY operator, route ORDER BY operator, route;"))
+
+#need unique bus numbers for each route for each day
+
+#now start the big loop
