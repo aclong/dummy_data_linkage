b475088416c0351b166aa8005f01c395f4c507af
aclong <37698923+aclong@users.noreply.github.com>
2020-04-07 11:28:27 +0100

    better pseudo code

--- dummy_trans_from_tt.R
+++ dummy_trans_from_tt.R
@@ -7,21 +7,40 @@
 # this should be done for each operator, for each route, for each day of week
 
 ###################
-# the function will download a list of distinct operator/route combinations
-
-# Initial SQL DB query:
-# using this list it will query the database to return bus journeys 
-# that are different "directions" in the tt 
-# and are as close as possible 
-# but do not overlap
-# the stop number in the series (ascending for each next stop in the route)
-# will be divided by 3 to approximate the fare stage
-#
-# R manipulation:
-# the stage numbers of each "inbound journey" will be reversed
+# Initial preparatory data
 # 
-# SQL output data creation:
-# a full version will be uploaded
-# a version of a random sample of 20% of rows will be uploaded
-# to represent similar coverage to the original data
+# 
+# R preparation input:
+# Assign period of dates - 3 months in which to carry this out
+# 
+# Initial SQL query:
+# download a list of distinct operator/route combinations
+# that occur in this period
+# 
+# R loop preparation:
+# Use the list of dates as the count/register
+# For each date, for each operator/route combo, loop the following:
+#
+## SQL DB query:
+## query the database to return all journeys from tt
+##
+### R loop:
+### select first "unused" journey of day
+### find next journey
+### that is different "direction" in the tt 
+### and is as close as possible 
+### but does not overlap
+### continue until there are no more non-overlapping opposite direction journeys that day/operator/route
+### make list of "used" journey numbers
+### 
+###
+## with data.table genreated by R Loop:
+## divide stop series number by 3 to approximate the fare stage
+## reverse the fare stages for each "inbound journey"
+## to mimic the fare stage relationship of transaction data
+## assign the data a unique "machine_ID" number
+##
+## SQL output data creation:
+## upload a full version to SQL db
+## upload a random sample of 20% of rows (to approximate transaction coverage) to another SQL db
 
